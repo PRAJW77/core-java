8ecfd6e2162510acfbabc764ff44cb5aa40ce8fb
Prajwal <prajwalsethuram@gmail.com>
2026-03-30 10:05:05 +0530

    Array and program aptitude

---
+++ Assignment9/Swap.java
@@ -0,0 +1,17 @@
+class Swap{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
